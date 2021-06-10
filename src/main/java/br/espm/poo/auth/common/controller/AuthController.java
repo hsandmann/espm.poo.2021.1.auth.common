be9980de4f4944195983e7020c4774b8fd67342d
Humberto Sandmann <hsandmann@ieee.org>
2021-06-10 15:13:30 -0300

    Initial version

--- src/main/java/br/espm/poo/auth/common/controller/AuthController.java
+++ src/main/java/br/espm/poo/auth/common/controller/AuthController.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.List;
-
 @FeignClient("auth-service")
 public interface AuthController {
 
